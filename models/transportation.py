414ff9d90cb1354ff4f5711809c00e268cf3c876
qimamhd <106440499+qimamhd@users.noreply.github.com>
2025-09-07 16:32:25 +0300

    update

--- models/transportation.py
+++ models/transportation.py
@@ -7,9 +7,20 @@ from datetime import datetime
 class transportation(models.Model):
     _inherit = 'trnsp.transportation.mst'
 
-       
+    
+    driver_btrip_selection = fields.Selection(
+        [('percentage', 'نسبة'), ('amount', 'مبلغ')], string='Status',
+         copy=False, index=True, tracking=3, default='percentage',required=True)
+         
+    driver_btrip_amount = fields.Float(string="عمولة السائق")
+
     booking_fees = fields.Float(string="رسوم حجز موعد")
     
     storage_fees = fields.Float(string="رسوم ساحة(تخزين)")
  
- 
+    
+    @api.onchange('driver_btrip_selection')
+    def reset_value_btrip(self):
+        for rec in self:
+            rec.driver_btrip_amount = 0
+            rec.company_driver_perc = 0
